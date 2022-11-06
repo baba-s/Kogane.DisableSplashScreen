7fad87f3a01f6a61276cc7e67de0ef2571c2ac1e
baba-s <shotababa1206@gmail.com>
2022-11-06 15:28:34 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.DisableSplashScreen
+# Kogane Disable Splash Screen
+
+ゲーム起動時に表示されるスプラッシュスクリーンを無効化するパッケージ
---
+++ Runtime/DisableSplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Kogane.Internal
+{
+    internal static class DisableSplashScreen
+    {
+        [RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.BeforeSplashScreen )]
+        private static void RuntimeInitializeOnLoadMethod()
+        {
+            Application.focusChanged += OnFocusChanged;
+
+            static void OnFocusChanged( bool isFocus )
+            {
+                Application.focusChanged -= OnFocusChanged;
+                SplashScreen.Stop( SplashScreen.StopBehavior.StopImmediate );
+            }
+        }
+    }
+}
